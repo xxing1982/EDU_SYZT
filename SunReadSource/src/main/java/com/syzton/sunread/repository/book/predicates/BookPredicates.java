4dcd665eea67ed746afb40177c0d3f4cd248b62c
jerrywalker0435 <sweet_grape@126.com>
2015-03-22 11:57:37 +0800

    book recommand

--- SunReadSource/src/main/java/com/syzton/sunread/repository/book/predicates/BookPredicates.java
+++ SunReadSource/src/main/java/com/syzton/sunread/repository/book/predicates/BookPredicates.java
@@ -1,6 +1,7 @@
 package com.syzton.sunread.repository.book.predicates;
 
 import com.mysema.query.types.Predicate;
+import com.syzton.sunread.model.book.BookExtra;
 import com.syzton.sunread.model.book.QBook;
 
 /**
@@ -15,4 +16,18 @@ public class BookPredicates {
                 .or(book.author.containsIgnoreCase(searchTerm)
                 .or(book.publisher.containsIgnoreCase(searchTerm)));
     }
+
+//    public static Predicate findByExtra(BookExtra extra){
+//        QBook book = QBook.book;
+//        Predicate predicate = book.;
+//        if(extra.getLevel()!= 0) // not all type
+//            predicate = book.extra.level.eq(extra.getLevel());
+//        else{
+////            predicate = book.extra;
+//        }
+//        return book.extra.level.eq(extra.getLevel())
+//                .andAnyOf(book.extra.language.eq(extra.getLanguage()))
+//                .andAnyOf(book.extra.testType.eq(extra.getTestType()))
+//                .andAnyOf(book.extra.literature.eq(extra.getLiterature()));
+//    }
 }
