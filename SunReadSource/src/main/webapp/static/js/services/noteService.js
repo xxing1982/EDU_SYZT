78b0533f733dbc06f9bcb0c196c334ed1b3b980f
chenty <eiddlechenjob@gmail.com>
2015-04-06 09:25:06 +0800

    Update noteserver

--- SunReadSource/src/main/webapp/static/js/services/noteService.js
+++ SunReadSource/src/main/webapp/static/js/services/noteService.js
@@ -3,7 +3,7 @@ var noteServices = angular.module('noteServices', ['ngResource', "nourConfig"]);
 // Note object(s)
 noteServices.factory('Note',['$resource', 'config',
 	function($resource, config){
-		return $resource("/api/notes?page=:page&size=:size&sortBy=:sort",
+		return $resource("/api/notes?page=:page&size=:size&sortBy=:sortBy",
                         {page:'@_page', size:'@_size', sortBy:"@_sort"},
                         {}
         );
