e70efede04acdad23450496edc896124373d7d99
chenty <eiddlechenjob@gmail.com>
2015-04-07 16:38:35 +0800

    Update dropdown

--- SunReadSource/src/main/webapp/static/js/controller/messageCenterCtrl.js
+++ SunReadSource/src/main/webapp/static/js/controller/messageCenterCtrl.js
@@ -1,5 +1,29 @@
 //messageCenter.js
 
 ctrls.controller("messageCenterController", ['$scope', function ($scope) {
-    $scope.messageCenter = '消息中心';
+    // Initlizate the dropdown statues
+    $scope.statuses = [{
+        id: 1,
+        name: "一年级上学期"        
+    }, {
+        id: 2,
+        name: "一年级下学期"        
+    }, {
+        id: 3,
+        name: "二年级上学期"        
+    }, {
+        id: 4,
+        name: "二年级下学期"        
+    }, {
+        id: 5,
+        name: "三年级上学期"        
+    }, {
+        id: 6,
+        name: "三年级下学期"        
+    }];
+    
+    // Choose the stutus by script 
+    // Once the dropdown option changed
+    // selected_status will changed simultaneity
+    $scope.selected_status = 3;
 }]);
