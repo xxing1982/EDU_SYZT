435a527ccb34c8e4c8c5f16934e381d0406ca673
chenty <eiddlechenjob@gmail.com>
2015-04-07 16:44:59 +0800

    Add example of 请选择of dropdown

--- SunReadSource/src/main/webapp/static/js/controller/messageCenterCtrl.js
+++ SunReadSource/src/main/webapp/static/js/controller/messageCenterCtrl.js
@@ -3,27 +3,26 @@
 ctrls.controller("messageCenterController", ['$scope', function ($scope) {
     // Initlizate the dropdown statues
     $scope.statuses = [{
-        id: 1,
-        name: "一年级上学期"        
+        id: 1       
     }, {
         id: 2,
-        name: "一年级下学期"        
+        name: "老校长"        
     }, {
         id: 3,
-        name: "二年级上学期"        
+        name: "二校长"        
     }, {
         id: 4,
-        name: "二年级下学期"        
+        name: "三校长"        
     }, {
         id: 5,
-        name: "三年级上学期"        
+        name: "斯校长"        
     }, {
         id: 6,
-        name: "三年级下学期"        
+        name: "三学期"        
     }];
     
     // Choose the stutus by script 
     // Once the dropdown option changed
     // selected_status will changed simultaneity
-    $scope.selected_status = 3;
+    $scope.selected_status = 1;
 }]);
