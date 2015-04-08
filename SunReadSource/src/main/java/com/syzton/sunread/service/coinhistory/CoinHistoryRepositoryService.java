c8bf0e884fb9ffe8ef25d1cb603a48762ced0357
chenty <eiddlechenjob@gmail.com>
2015-04-08 23:02:53 +0800

    Update modal

--- SunReadSource/src/main/java/com/syzton/sunread/service/coinhistory/CoinHistoryRepositoryService.java
+++ SunReadSource/src/main/java/com/syzton/sunread/service/coinhistory/CoinHistoryRepositoryService.java
@@ -1,9 +1,15 @@
 package com.syzton.sunread.service.coinhistory;
 
 import java.util.List;
+
 import com.syzton.sunread.exception.common.NotFoundException;
 import com.syzton.sunread.model.coinhistory.CoinHistory;
+import com.syzton.sunread.model.coinhistory.CoinHistory.CoinType;
+import com.syzton.sunread.model.user.Student;
+import com.syzton.sunread.model.user.UserStatistic;
 import com.syzton.sunread.repository.coinhistory.CoinHistoryRepository;
+import com.syzton.sunread.repository.user.StudentRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +32,25 @@ public class CoinHistoryRepositoryService implements CoinHistoryService {
         this.repository = repository;
     }
 
+    
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public void StudentRepositoryService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public CoinHistory add(CoinHistory add) {
         LOGGER.debug("Adding a new coinHistory entry with information: {}", add);
+        Student student = studentRepository.findOne(add.getUser().getId());
+        UserStatistic statistic = student.getStatistic();
+        if (add.getCoinType() == CoinType.IN) {
+        	statistic.setCoin( statistic.getCoin() + add.getNum() );
+        } else {
+        	statistic.setCoin( statistic.getCoin() - add.getNum() );
+        }
+        studentRepository.save(student);
         return repository.save(add);
     }
     
