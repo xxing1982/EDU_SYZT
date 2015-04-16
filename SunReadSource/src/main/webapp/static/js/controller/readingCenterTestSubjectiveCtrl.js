99bf32bcbc0553ee1e673f8701c5836c87529873
chyang001 <yangchanghui1991@sina.com>
2015-04-17 00:58:16 +0800

    think exam

--- SunReadSource/src/main/webapp/static/js/controller/readingCenterTestSubjectiveCtrl.js
+++ SunReadSource/src/main/webapp/static/js/controller/readingCenterTestSubjectiveCtrl.js
@@ -2,32 +2,35 @@
 
 ctrls.controller("readingCenterTestSubjectiveController", ['$scope', '$rootScope', 'ThinkExam', function ($scope, $rootScope, ThinkExam) {
 	if ($rootScope.exam == undefined) {
-	window.location.href="/protype/index.html#/readingCenter/myBookshelf";
+		window.location.href="/protype/index.html#/readingCenter/myBookshelf";
 	};
 	$scope.bookName = $rootScope.exam.bookName;
 
 	ThinkExam.get($rootScope.exam.bookId, function(data){
-		$scope.questions = data;
+		ThinkExam.get($rootScope.exam.bookId, function(dataquestion){
+			$scope.questions = data;
 
-		$scope.finishExam = function(){
-			$scope.myAnswer = {
-				"bookId": $rootScope.exam.bookId,
-				"studentId": $rootScope.id,
-				"examType": "THINK"
-			};
-			$scope.myAnswer.answers = new Array();
-			$scope.myAnswer.questions = $scope.questions;
-			for (var i = 0; i < $scope.questions.length; i++) {
-				answer = {};
-				answer.question = $scope.questions[i];
-				answer.studentId = $rootScope.id;
-				answer.content = $scope.questions[i].content;
+			$scope.finishExam = function(){
+				$scope.myAnswer = {
+					"bookId": $rootScope.exam.bookId,
+					"studentId": $rootScope.id,
+					"examType": "THINK"
+				};
+				$scope.myAnswer.answers = new Array();
+				$scope.myAnswer.questions = dataquestion;
+				for (var i = 0; i < $scope.questions.length; i++) {
+					answer = {};
+					answer.question = dataquestion[i];
+					answer.studentId = $rootScope.id;
+					answer.content = $scope.questions[i].content;
 
-				$scope.myAnswer.answers[i] = answer;
-			};
-			ThinkExam.submitExam($scope.myAnswer, function(examData){
-				console.log(examData);
-			})
-		}
+					$scope.myAnswer.answers[i] = answer;
+				};
+
+				ThinkExam.submitExam($scope.myAnswer, function(examData){
+					console.log(examData);
+				})
+			}
+		})
 	})
-}]);
+	}]);
