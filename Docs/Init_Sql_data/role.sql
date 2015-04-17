a2a24f5431d1aa176f809b11ae4928244ffd2f83
jerrywalker0435 <sweet_grape@126.com>
2015-04-17 22:24:01 +0800

    add init sql

---
+++ Docs/Init_Sql_data/role.sql
@@ -0,0 +1,4 @@
+INSERT INTO sunreading.role (id, creation_time, description, rolename) VALUES (1, '2015-03-28 19:20:43', 'User', 'ROLE_USER');
+INSERT INTO sunreading.role (id, creation_time, description, rolename) VALUES (2, '2015-03-28 19:20:43', 'User', 'ROLE_TEACHER');
+INSERT INTO sunreading.role (id, creation_time, description, rolename) VALUES (3, '2015-03-28 19:20:43', 'User', 'ROLE_STUDENT');
+INSERT INTO sunreading.role (id, creation_time, description, rolename) VALUES (4, '2015-03-28 19:20:43', 'User', 'ROLE_PARENT');
