918919e15183b2ca19f8062d58685bc1a3330d89
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2015-06-19 13:32:52 +0800

    fix bugs

--- SunReadSource/src/main/java/com/syzton/sunread/repository/SemesterRepository.java
+++ SunReadSource/src/main/java/com/syzton/sunread/repository/SemesterRepository.java
@@ -7,15 +7,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
+import com.syzton.sunread.model.book.Book;
 import com.syzton.sunread.model.organization.Campus;
 import com.syzton.sunread.model.semester.Semester;
 
-public interface SemesterRepository extends JpaRepository<Semester,Long>{
+public interface SemesterRepository extends JpaRepository<Semester,Long>,QueryDslPredicateExecutor<Semester>{
+	
 	@Query("SELECT Distinct(s) FROM Semester s WHERE s.startTime>(:time) AND s.endTime<(:time) AND s.campus=(:campus)")
 	Semester findByTimeAndCampus(@Param("time")DateTime time,@Param("campus")Campus campus);
-	@Query("SELECT Distinct(s) FROM Semester s WHERE s.startTime>(:startTime) AND s.endTime<(:endTime) ORDER BY s.startTime DESC AND s.campus=(:campus)")
+	@Query("SELECT Distinct(s) FROM Semester s WHERE s.campus=(:campus) AND s.startTime>(:startTime) AND s.endTime<(:endTime) ORDER BY s.startTime DESC ")
 	ArrayList<Semester> findByDuration(@Param("startTime")DateTime startTime,@Param("endTime")DateTime endTime,@Param("campus")Campus campus);
 	
 	Page<Semester> findByCampus(Campus campus, Pageable pageable);
