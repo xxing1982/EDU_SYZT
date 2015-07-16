cf3c72d9cf0bf2d8929b33c9e821916dcead6acf
李永楠|Morgan|Léon <14126123@bjtu.edu.cn>
2015-07-16 23:49:07 +0800

    booktag Service

--- SunReadSource/src/main/webapp/teacher/static/js/services/booktagService.js
+++ SunReadSource/src/main/webapp/teacher/static/js/services/booktagService.js
@@ -6,4 +6,14 @@ booktagServices.factory('Booktag',['$resource', 'config',
 		return $resource(config.HOST + "booktags",
             {}, {}
         );
-	}]); 
+	}]);
+
+//Get book By tag
+booktagServices.factory('GetBooksByTag',['$resource', 'config',
+	function($resource, config){
+		return $resource(config.HOST + "books/tags?page=:page&size=:size&sortBy=:sortBy&
+													lesson=:lesson&grade=:grade&subject=:subject&chapter=:chapter&theme=:theme",
+            {page:'@_page', size:'@_size',sortBy:'@_sortBy',lesson:'@_lesson',grade:'@_grade'
+						,subject:'@_subject',chapter:'@_chapter',theme:'@_theme'}, {}
+        );
+	}]);)
