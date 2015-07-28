d102e51af17a058deed1fc8ebc8b8bca939de872
jerrywalker0435 <sweet_grape@126.com>
2015-07-28 22:38:55 +0800

    message read option

--- SunReadSource/src/main/java/com/syzton/sunread/service/region/RegionRepositoryService.java
+++ SunReadSource/src/main/java/com/syzton/sunread/service/region/RegionRepositoryService.java
@@ -90,6 +90,10 @@ public class RegionRepositoryService implements RegionService {
 		Region model = findOne(id);
 
 		LOGGER.debug("Found a note entry: {}", model);
+		model.setParent(updated.getParent());
+		model.setRegionType(updated.getRegionType());
+		model.setSubRegion(updated.getSubRegion());
+		model.setName(updated.getName());
 		regionRepo.save(model);
 		return updated;
 	}
